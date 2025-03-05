235ddb423883d7dbe09e50b403532ed6b039da82
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-03-05 14:52:03 +0600

    Added UrlConstraint

--- AgroNeed/src/main/java/com/nipa/agroneed/utils/UrlConstraint.java
+++ AgroNeed/src/main/java/com/nipa/agroneed/utils/UrlConstraint.java
@@ -6,6 +6,11 @@ public final class UrlConstraint {
     public static class AgroNeed{
         public static final String ROOT = "/AgroNeed";
         public static final String CREATE = "/create";
+        public static final String GET_ALL = "/getAll";
+        public static final String GET_BY_ID = "/get/{id}";
+        public static final String GET_BY_ParentId = "/get/{parentId}";
+        public static final String DELETE_BY_ID = "/delete/{id}";
+        public static final String EDIT_BY_ID = "/edit/{id}";
 
     }
 }

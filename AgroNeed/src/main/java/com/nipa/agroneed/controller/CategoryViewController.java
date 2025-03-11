f26a7f8d651b05014528d5d47ea23ff799f2b0b0
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-03-11 19:39:42 +0600

    added new view controller for categories

--- AgroNeed/src/main/java/com/nipa/agroneed/controller/CategoryViewController.java
+++ AgroNeed/src/main/java/com/nipa/agroneed/controller/CategoryViewController.java
@@ -1,8 +1,19 @@
 package com.nipa.agroneed.controller;
 
 
+import com.nipa.agroneed.service.CategoriesService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CategoryViewController {
+    private final CategoriesService categoriesService;
+
+    public CategoryViewController(CategoriesService categoriesService) {
+        this.categoriesService = categoriesService;
+    }
+    @GetMapping("/addCategory")
+    public String addCategory() {
+        return "add_Category";
+    }
 }

a95cc207770e69580f427b6627d16c68a13a3b63
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-03-13 20:51:45 +0600

    added new view controller for categories

--- AgroNeed/src/main/java/com/nipa/agroneed/utils/UrlConstraint.java
+++ AgroNeed/src/main/java/com/nipa/agroneed/utils/UrlConstraint.java
@@ -3,14 +3,17 @@ package com.nipa.agroneed.utils;
 public final class UrlConstraint {
     public UrlConstraint() {
     }
-    public static class AgroNeed{
-        public static final String ROOT = "/AgroNeed";
+    public static class Categories {
+        public static final String ROOT = "/Categories";
         public static final String CREATE = "/create";
         public static final String GET_ALL = "/getAll";
         public static final String GET_BY_ID = "/get/{id}";
         public static final String GET_BY_ParentId = "/get/{parentId}";
         public static final String DELETE_BY_ID = "/delete/{id}";
         public static final String EDIT_BY_ID = "/edit/{id}";
+
+    }
+    public static class Products{
 
     }
 }

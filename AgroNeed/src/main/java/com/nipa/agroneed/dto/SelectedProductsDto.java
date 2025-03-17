2e753fbb87a9a96e81416c0cbfc83be0e4465b34
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-03-17 20:21:54 +0600

    added new dto selectedProducts

--- AgroNeed/src/main/java/com/nipa/agroneed/dto/SelectedProductsDto.java
+++ AgroNeed/src/main/java/com/nipa/agroneed/dto/SelectedProductsDto.java
@@ -3,7 +3,8 @@ package com.nipa.agroneed.dto;
 import lombok.Data;
 
 @Data
-public class ProductDto extends BaseDto {
+public class SelectedProductsDto {
+    private Long selectedCategoryId;
     private String name;
     private String description;
     private Double price;

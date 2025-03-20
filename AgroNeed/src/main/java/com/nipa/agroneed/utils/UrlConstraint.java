3ad96e35d325335d8012b8e9c2b587a1b11b3435
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-03-20 13:42:15 +0600

    Edited UrlContrsint

--- AgroNeed/src/main/java/com/nipa/agroneed/utils/UrlConstraint.java
+++ AgroNeed/src/main/java/com/nipa/agroneed/utils/UrlConstraint.java
@@ -7,8 +7,9 @@ public final class UrlConstraint {
         public static final String ROOT = "/Categories";
         public static final String CREATE = "/create";
         public static final String GET_ALL = "/getAll";
-        public static final String GET_BY_ID = "/get/{id}";
-        public static final String GET_BY_ParentId = "/get/{parentId}";
+        public static final String GET_ALL_PARENT_CATEGORY = "/getAllParentCategory";
+        public static final String GET_BY_ID = "/getById/{id}";
+        public static final String GET_BY_ParentId = "/getByParentId/{parentId}";
         public static final String DELETE_BY_ID = "/delete/{id}";
         public static final String EDIT_BY_ID = "/edit/{id}";
 
@@ -18,5 +19,10 @@ public final class UrlConstraint {
         public static final String CREATE = "/create";
         public static final String GET_ALL = "/getAll";
 
+    }
+    public static class Users{
+        public static final String ROOT = "/Users";
+        public static final String CREATE = "/create";
+        public static final String GET_ALL = "/getAll";
     }
 }

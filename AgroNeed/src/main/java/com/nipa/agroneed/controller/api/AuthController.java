0e192a19cc59d0db90abe4ca936d9ceeb7c9b967
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-03-25 15:49:44 +0600

    added new API AddRole

--- AgroNeed/src/main/java/com/nipa/agroneed/controller/api/AuthController.java
+++ AgroNeed/src/main/java/com/nipa/agroneed/controller/api/AuthController.java
@@ -1,6 +1,7 @@
 package com.nipa.agroneed.controller.api;
 
 
+import com.nipa.agroneed.dto.AddRoleDto;
 import com.nipa.agroneed.dto.LoginDto;
 import com.nipa.agroneed.dto.Response;
 import com.nipa.agroneed.dto.UserDto;
@@ -31,5 +32,12 @@ public class AuthController {
     public Response register(@RequestBody UserDto userDto) {
         return authService.register(userDto);
     }
+    @PostMapping("/addRole")
+    public Response addRole(@RequestBody AddRoleDto addRoleDto) {
+        return authService.addRole(addRoleDto);
+    }
+
+
+
 
 }

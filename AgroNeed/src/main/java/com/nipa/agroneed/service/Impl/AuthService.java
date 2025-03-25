43f539f6f5850c25523cd5a7432cb633b723c420
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-03-25 15:50:05 +0600

    added new API AddRole

--- AgroNeed/src/main/java/com/nipa/agroneed/service/Impl/AuthService.java
+++ AgroNeed/src/main/java/com/nipa/agroneed/service/Impl/AuthService.java
@@ -2,11 +2,10 @@ package com.nipa.agroneed.service.Impl;
 
 
 import com.nipa.agroneed.config.JwtTokenProvider;
-import com.nipa.agroneed.dto.LoginDto;
-import com.nipa.agroneed.dto.LoginResponseDto;
-import com.nipa.agroneed.dto.Response;
-import com.nipa.agroneed.dto.UserDto;
+import com.nipa.agroneed.dto.*;
+import com.nipa.agroneed.entity.Role;
 import com.nipa.agroneed.entity.User;
+import com.nipa.agroneed.repository.RoleRepository;
 import com.nipa.agroneed.repository.UserRepository;
 import com.nipa.agroneed.utils.ResponseBuilder;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,18 +16,22 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AuthService {
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
-    public AuthService(UserRepository userRepository, AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, PasswordEncoder passwordEncoder) {
+    public AuthService(UserRepository userRepository, AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.authenticationManager = authenticationManager;
         this.jwtTokenProvider = jwtTokenProvider;
         this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
     }
 
     public Response login(LoginDto loginDto, HttpServletRequest httpServletRequest) {
@@ -42,7 +45,7 @@ public class AuthService {
                         loginDto.getPassword()));
         if (authentication.isAuthenticated()) {
             LoginResponseDto loginResponseDto = new LoginResponseDto();
-                loginResponseDto.setToken(jwtTokenProvider.generateToken(authentication, httpServletRequest));
+            loginResponseDto.setToken(jwtTokenProvider.generateToken(authentication, httpServletRequest));
             loginResponseDto.setUsername(user.getName());
             return ResponseBuilder.getFailResponse(HttpStatus.OK,
                     loginResponseDto, "Login Successful");
@@ -68,4 +71,26 @@ public class AuthService {
         }
         return ResponseBuilder.getFailResponse(HttpStatus.BAD_REQUEST, null, "Exist user using this phone");
     }
+
+    public Response addRole(AddRoleDto addRoleDto) {
+        User user = userRepository.findByIdAndStatus(addRoleDto.getUserId(), 1);
+        if (user == null) {
+            return ResponseBuilder.getFailResponse(HttpStatus.BAD_REQUEST, null, "User does not exist");
+        }
+        Role role = roleRepository.findByNameAndStatus(addRoleDto.getRoleName(), 1);
+        if (role == null) {
+            role = new Role();
+            role.setName(addRoleDto.getRoleName());
+            role.setStatus(1);
+            role = roleRepository.save(role);
+        }
+        List<Role> roles = user.getRoles();
+        roles.add(role);
+
+        user.setRoles(roles);
+        User savedUser = userRepository.save(user);
+        return ResponseBuilder.getSuccessResponse(HttpStatus.CREATED, savedUser,
+                "Successfully added role");
+    }
+
 }

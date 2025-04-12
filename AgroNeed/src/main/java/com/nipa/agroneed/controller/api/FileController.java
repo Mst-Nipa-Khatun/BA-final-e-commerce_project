54f49fe0996bb167f9cddd28b070b8920b1b8f56
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-04-12 13:53:33 +0600

    complete dashboard image show

--- AgroNeed/src/main/java/com/nipa/agroneed/controller/api/FileController.java
+++ AgroNeed/src/main/java/com/nipa/agroneed/controller/api/FileController.java
@@ -16,9 +16,21 @@ public class FileController {
     @Value("${file.upload-dir}")
     private String imageStoreLocation;
 
+    @Value("${file.static-dir}")
+    private String applicationStaticImage;
+
     @GetMapping("/images/{filename}")
     public ResponseEntity<Resource> getImage(@PathVariable String filename) {
         File file = new File(imageStoreLocation + "/" + filename);
+        if (!file.exists()) {
+            return ResponseEntity.notFound().build();  // Return 404 if image is not found
+        }
+        Resource resource = new FileSystemResource(file);
+        return ResponseEntity.ok().body(resource);  // Return image as response
+    }
+    @GetMapping("/application-static-image/{filename}")
+    public ResponseEntity<Resource> applicationStaticImage(@PathVariable String filename) {
+        File file = new File(applicationStaticImage + "/" + filename);
         if (!file.exists()) {
             return ResponseEntity.notFound().build();  // Return 404 if image is not found
         }

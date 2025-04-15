4a48f4594ccd049e0d3b4163bdc68c262d5e2d29
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-04-15 22:14:20 +0600

    Finally My e-Commerc prjoect Agronned done

--- AgroNeed/src/main/java/com/nipa/agroneed/config/SecurityConfig.java
+++ AgroNeed/src/main/java/com/nipa/agroneed/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
                                 "/webjars/**", "/css/**", "/images/**", "/favicon.ico",
                                 "/favicon.png", "/shared/**", "/webjars/AdminLTE/**",
                                 "/Categories/getAll","/users/create","/shoppingCart","/shoppingCart/getAll",
-                                "/orders/placeOrder","/orders/getAll","/viewAllOrders","/orders/ordersUpdate",
+                                "/orders/placeOrder","/orders/getAll","/viewAllOrders","/orders/ordersUpdate","/application-static-image/**",
                                 "/Products/create"//todo will need to authenticated dynamically
                         ).permitAll()
                         .anyRequest().authenticated()
